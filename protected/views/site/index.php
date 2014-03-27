05833ab0f3f96aee035b30eea192949d11d6a8aa
helenkappesmicro-p <helen.kappes@exertismicro-p.co.uk>
2014-03-27 13:53:12 +0000

    Added link for april incentive

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,4 +6,6 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>The applications allows the management of winning numbers, prize creation and user entries.</p>
+<p>The applications allows the management of winning numbers, prize creation and user entries for
+</p><a href='http://www.exertismicro-p.co.uk/cmcPage.asp?idPage=27489' >April Incentive</a>
+
